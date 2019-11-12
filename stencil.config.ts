af1906662dc37886ea845c503df4a8838319680e
Josh Beckwith <josh.beckwith@gmail.com>
2019-11-12 09:34:06 -0500

    publish

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,13 +3,14 @@ import { sass } from '@stencil/sass'
 
 // https://stenciljs.com/docs/config
 export const config: Config = {
-  outputTargets: [{
-    type: 'www',
-    serviceWorker: null,
-    // baseUrl: 'https://positlabs.github.io/temporalis/',
-  }],
+  outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: null,
+      baseUrl: 'temporalis',
+    },
+  ],
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.scss',
   plugins: [sass()],
 }
-

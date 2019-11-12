98994eeaf48da68fe04a3e745efce6317ea52147
Josh Beckwith <josh.beckwith@gmail.com>
2019-11-12 09:42:45 -0500

    publish

--- stencil.config.ts
+++ stencil.config.ts
@@ -7,7 +7,7 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null,
-      baseUrl: 'temporalis',
+      baseUrl: 'https://positlabs.github.io/temporalis/',
     },
   ],
   globalScript: 'src/global/app.ts',

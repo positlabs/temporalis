73fb3f3d986000041378c6e1b3ed1196326220b7
Josh Beckwith <josh.beckwith@gmail.com>
2019-11-15 10:20:31 -0500

    publish

---
+++ docs/sw.js
@@ -0,0 +1,56 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "86e6b9ae7540df39574602fe690c6807"
+  },
+  {
+    "url": "build/index.esm.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "build/p-3d72e23c.js"
+  },
+  {
+    "url": "build/p-5wz37z7f.css"
+  },
+  {
+    "url": "build/p-d0882b30.js"
+  },
+  {
+    "url": "build/p-d8631f0b.js"
+  },
+  {
+    "url": "build/p-lmwznh8b.entry.js"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "e7d2510de026e126b1a149d31f6331ad"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
